70fc460c60fda71472e549836f684b4f87ac37c7
Ville Strengell <villes-97@hotmail.com>
2024-11-05 11:33:48 +0200

    dynamic sizing of logo

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -8,8 +8,8 @@ export default function LandingPage() {
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <img src="/pawn.svg" alt="Logo" className="h-8 w-8 text-primary" />
-          <span className="ml-2 text-2xl font-bold">Pion Blanc</span>
+          <img src="/pawn.svg" alt="Logo" className="h-7 w-7 md:h-7 md:w-7 lg:h-8 lg:w-8 text-primary" />
+          <span className="ml-2 text-lg md:text-xl lg:text-2xl font-bold">Pion Blanc</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link className="text-sm font-medium hover:underline underline-offset-4" href="#features">
