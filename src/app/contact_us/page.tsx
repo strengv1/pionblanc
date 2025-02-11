09c4c71ac529ed3c4a76e689c51f83a8ed8a1022
Ville Strengell <villes-97@hotmail.com>
2025-02-11 13:44:26 +0200

    small margin fix

--- src/app/contact_us/page.tsx
+++ src/app/contact_us/page.tsx
@@ -12,7 +12,7 @@ export default function ContactUs() {
   return (
     <div className="max-w-screen-md w-full mx-auto my-24 px-4 flex-1">
       <div className="py-20 lg:py-32 text-left">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold mb-2">
           {t("title")}
         </h1>
         <p className="md:text-md lg:text-lg text-left text-balance">
--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -6,7 +6,7 @@ export default function Privacy() {
   return (
     <div className="max-w-screen-md w-full mx-auto my-24 px-4 flex-1">
       <div className="py-20 lg:py-32 text-left">
-        <h1 className="text-3xl font-bold my-4">
+        <h1 className="text-3xl font-bold">
           {t("title")}
         </h1>
         <p>
