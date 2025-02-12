b55cb4014a11685dcc0ade052f8b54ac0b7d2553
Ville Strengell <villes-97@hotmail.com>
2025-02-12 15:35:52 +0200

    improve testimonials for mobile

--- src/app/_components/TestimonialsSection.tsx
+++ src/app/_components/TestimonialsSection.tsx
@@ -7,9 +7,10 @@ export default function TestimonialsSection() {
 
   return (
     <section id="testimonials"
-      className="testimonials-section w-full mx-auto px-4 py-28 md:py-24 lg:py-32 text-secondary md:mt-[200px] max-w-screen-xl"
+      className="testimonials-section w-full mx-auto px-4 py-20 md:py-24 lg:py-32 text-secondary md:mt-[200px] max-w-screen-xl"
     >
-      <div className="flex gap-10 mx-5">
+      {/* Desktop */}
+      <div className="hidden md:flex gap-10 mx-5">
         <div className="w-full md:flex text-center self-center justify-center gap-5">
           <span className="text-5xl md:text-8xl">
             5/5
@@ -38,6 +39,38 @@ export default function TestimonialsSection() {
             loading="lazy"
           />
         </div>
+      </div>
+
+      {/* Mobile */}
+      <div className="md:hidden flex flex-col gap-10 mx-5">
+        <div className="flex flex-col gap-6">
+          <Quote width="2rem" height="2rem"/>
+          <p className="text-md italic">
+            {t("from_daniels")}
+          </p>
+
+          <Image
+            src="/logos/daniels_logo_testimonials.png"
+            alt={`- Daniel's`}
+            width={150}
+            height={200}
+            className="object-contain w-32 h-10"
+            loading="lazy"
+          />
+        </div>
+        <div className="w-full flex text-center self-center justify-center gap-5">
+          <span className="text-5xl ">
+            5/5
+          </span>
+          <div className="flex self-center justify-center">
+            <Star fill="secondary"/>
+            <Star fill="secondary"/>
+            <Star fill="secondary"/>
+            <Star fill="secondary"/>
+            <Star fill="secondary"/>
+          </div>
+        </div>
+
       </div>
     </section>
   );
