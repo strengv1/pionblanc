612d21d110ecca31a068ba18864563456fc8f07c
Ville Strengell <villes-97@hotmail.com>
2025-02-17 22:19:56 +0200

    new and improved comparison section

--- src/app/_components/FinalHook.tsx
+++ src/app/_components/FinalHook.tsx
@@ -10,7 +10,7 @@ export default function FinalHookSection() {
 
   return (
     <section className="
-      additional-info-section w-full mt-20 md:mt-40 mb-20 lg:mt-60 flex flex-col gap-32 mx-auto items-center text-center
+      additional-info-section w-full mt-20 md:mt-40 mb-28 lg:mt-40 flex flex-col gap-32 mx-auto items-center text-center
     ">
       <div className="flex py-10 md:py-20 md:px-32 rounded-3xl">
         <div className="w-full flex flex-col gap-4 lg:gap-10">
